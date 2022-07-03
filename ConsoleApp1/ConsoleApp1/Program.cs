9d04826602c540d955535684e24b2a568396fb54
mahammadalirk <mahammadalirk@code.edu.az>
2022-07-03 23:50:57 +0400

    ConsoleApp1 file

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n, p;
+
+            n = 9;
+            p = 3;
+            Console.WriteLine("Regem:" + n);
+            Console.WriteLine("Regemin Guvveti:" + p);
+
+            double res = Math.Pow(n, p);
+            Console.WriteLine("Cavab = {0}", res);
+            Console.ReadLine();
+        }
+    }
+}
+    
